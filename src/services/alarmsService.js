12f19d46beb95d5a081ac855a90a2f7c9c155515
Wilfredo Alcala <alcalawilfre@gmail.com>
2019-04-26 00:14:16 -0300

    Add mocked methods

--- src/services/alarmsService.js
+++ src/services/alarmsService.js
@@ -1,9 +1,34 @@
 const SymbolWatcher = require('./SymbolWatcher');
 
-const XBTUSD = new SymbolWatcher('XBTUSD');
+
 
 class AlarmsService {
-    // TODO: return methods to manage watcher instances
+    // TODO: create methods to manage watcher instances
+
+    constructor() {
+        this.watchers = [];
+    }
+
+    createNewWatcher(symbol) {
+        // TODO: Validate if symbol already exists
+        // TODO: Allow names for watcher objects
+        const watcher = new SymbolWatcher(symbol);
+        this.watchers.push(watcher);
+        return watcher;
+    }
+
+    createAlarm(name, symbol, targetPrice) {
+        // TODO: Validate if alarm name already exists
+        // TODO: return a parsed alarm object
+        // TODO: Create alarm
+        const id = `${symbol}_${targetPrice}`
+        return {
+            id,
+            name,
+            symbol,
+            targetPrice
+        }
+    }
 }
 
 module.exports = new AlarmsService();
